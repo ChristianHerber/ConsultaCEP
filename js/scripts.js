d2f0ef9d2a632a39b7fb28d39e4a03e9e849b403
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-05-20 16:06:54 -0400

    Show data in table

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
 function consultaCep(){
-    let cep = document.querySelector('#cep').value
+    let cepDigitado = document.querySelector('#cepDigitado').value
     $.ajax({
-        url: `https://viacep.com.br/ws/${cep}/json/`,
+        url: `https://viacep.com.br/ws/${cepDigitado}/json/`,
         type: "get",
         success: function(response){
             console.log(response)
+            let cep = document.getElementById('cep')
+            let localidade = document.getElementById('localidade')
+            let uf = document.getElementById('uf')
+            let ibge = document.getElementById('ibge')
+            let logradouro = document.getElementById('logradouro')
+            let complemento = document.getElementById('complemento')
+            let bairro = document.getElementById('bairro')
+            let gia = document.getElementById('gia')
+            let siafi = document.getElementById('siafi')
+
+            cep.innerHTML = response.cep
+            localidade.innerHTML = response.localidade
+            uf.innerHTML = response.uf
+            ibge.innerHTML = response.ibge
+            logradouro.innerHTML = response.logradouro
+            complemento.innerHTML = response.complemento
+            bairro.innerHTML = response.bairro
+            gia.innerHTML = response.gia
+            siafi.innerHTML = response.siafi
         }
     })
 }
